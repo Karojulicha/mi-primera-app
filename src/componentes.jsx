e0b2ddbffff9e9185c3f134100058429f33eef5f
Karol Chaparro <karolgomez171@gmail.com>
2022-06-30 14:05:19 -0500

    parrafo

---
+++ src/componentes.jsx
@@ -0,0 +1,10 @@
+import { Componentes } from "react";
+class Componentes extends Componentes{
+    render(){
+        return (<div>
+            <h1> Hola mi primera app</h1>
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Id odio est minus fugiat nemo, officia sed perspiciatis aspernatur quaerat voluptatem omnis nisi animi dolores unde cum culpa corrupti doloribus. Quas?</p>
+        </div>)
+    }
+}
+export default Componentes
